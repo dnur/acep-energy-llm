f25933792c650dd109ab056da1dd90b683cdeb34
Najib Haidar <najibrhaidar@gmail.com>
2024-05-28 22:45:10 -0700

    Delete Backend/google_drive_top.py

--- Backend/google_drive_top.py
+++
@@ -1,8 +0,0 @@
-from google_drive_helper import pdf_to_json, json_to_vector
-
-bucket1 = '1-OcjRs7SFL-J9i1ekKe5CQElPlZ0Utcs'
-bucket2 = '1N6ShPsKoxcUtn-Ky75Uk4J3qgQPXo2hw'        
-bucket3 = '1jP5PfAKLwdGDQDvN_TihNIEBcsGASoe_'
-
-pdf_to_json(bucket1, bucket2, bucket3)
-# json_to_vector(bucket3) #TODO
